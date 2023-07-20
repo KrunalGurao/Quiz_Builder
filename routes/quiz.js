8edac7ea42e2390efe18fcfd801fbf060cddeb88
KrunalGurao <krunalgurao1@gmail.com>
2023-07-20 20:20:35 +0530

    Done

--- routes/quiz.js
+++ routes/quiz.js
@@ -6,65 +6,61 @@ const cors = require('cors')
 
 quizRouter.use(cors())
 
-quizRouter.post('/create', async (req, res) => {
+quizRouter.post("/create", async (req, res) => {
   try {
-    const { creator, title, description, questions } = req.body;
-    const quiz = new Quiz({ creator, title, description, questions });
-    res.status(200).json(quiz);
+      const { creator, title, description, questions, leaderboard } = req.body
+
+      const addQuiz = new Quiz({ creator, title, description, questions, leaderboard: [] })
+      await addQuiz.save()
+      res.status(200).send({ msg: "Quiz Added sucessfully", ok: true })
   } catch (error) {
-    res.status(400).json(error);
+      res.status(400).send({ msg: error.message })
   }
-});
+})
+
+//*************************************************************************************************
 
 
-
-
-//*********************************************************************************************** */
-
-
-
-quizRouter.delete('/:id', async (req, res) => {
+quizRouter.delete("/delete/:id/:email", async (req, res) => {
   try {
-    const { id } = req.params;
-    await Quiz.findByIdAndDelete(id);
-    res.status(200).end();
+      const { id, email } = req.params
+      const user = await Quiz.findOne({ _id: id })
+
+      if (user.creator != email) {
+          return res.status(400).send({ msg: "You are not authorized" })
+      }
+      const deletequiz = await Quiz.findByIdAndDelete({ _id: id })
+      return res.status(200).send({ msg: "Quiz Deleted" })
   } catch (error) {
-    res.status(400).json({ error: 'Error occured' });
+      return res.status(400).send({ msg: error.message })
   }
-});
+})
 
 
-
-//*********************************************************************************************** */
-
+//*************************************************************************************************
 
 
-quizRouter.put('/:id', async (req, res) => {
+quizRouter.get("/", async (req, res) => {
   try {
-    const { id } = req.params;
-    const { title, description } = req.body;
-    const quiz = await Quiz.findByIdAndUpdate(
-      id,
-      { title, description },
-      { new: true }
-    );
-    res.json(quiz);
+      const data = await Quiz.find()
+      res.status(200).send(data)
   } catch (error) {
-    res.status(400).json({ error: 'Error occured' });
+      res.status(400).send({ msg: error.message })
   }
-});
+})
 
 
-//************************************************************************************** */
+//*************************************************************************************************
 
 
-quizRouter.get('/quiz',async (req, res) => {
-    try{
-        const quizzes = await Quiz.find();
-        res.status(200).json(quizzes);
-    } catch (error) {
-        res.status(400).json('Failed to retrieve quiz data');
-    }
+quizRouter.get("/particular", async (req, res) => {
+  try {
+      const quizId = req.query.id
+      const quizdata = await Quiz.findById(quizId)
+      return res.status(200).send(quizdata)
+  } catch (error) {
+      return res.status(400).send({ msg: error.message })
+  }
 })
 
 
